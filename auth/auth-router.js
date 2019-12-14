f6749bb8e03eeed7ff29a7bde2a85cd949206041
Dominick Bruno <dbruno@unseen.is>
2019-12-14 15:43:11 -0500

    added userHelper and imports to auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const Users = require('./config/userHelper');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('./config/secrets.js');
 
 router.post('/register', (req, res) => {
   // implement registration
